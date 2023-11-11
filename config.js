0c59129ae4953b048bd57d0f2891ccf894d4fe72
LZRSJOKER <95168467+LZRSJOKER@users.noreply.github.com>
2023-11-11 16:45:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['917561815867', '___𝙸𝚃𝚊𝙲𝙷𝙸__🪬', true],
+  ['916282244301', '𝐘𝚯𝚮𝚫𝚫𝚴', true], 
+  ['17057710927','𝑆𝛨𝛪𝛮𝛭É𝛮 𝑇𝛥𝛫𝛯𝛧Ō',true] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['917561815867','916282244301'] 
+global.prems = ['917561815867','916282244301','17057710927'] 
+global.allowed =  ['917561815867','916282244301','17057710927']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,26 +50,26 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝐘𝚯𝚮𝚫𝚫𝚴'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
+global.packname = '☮️🕊️' 
+global.author = '___𝙸𝚃𝚊𝙲𝙷𝙸__🪬' 
+global.menuvid = 'https://telegra.ph/file/𝑈𝐶𝛨𝛪𝛨𝛥-𝛪𝑇𝛥𝐶𝛨𝛪-11-11-2.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/cr7
+global.dygp = 'https://chat.whatsapp.com'
+global.fgsc = 'https://github.com' 
+global.fgyt = 'https://youtube.com'
+global.fgpyp = 'https://youtube.com'
+global.fglog = 'https://i.imgur.com/E3ZX2PM.jpeg' 
+global.thumb = fs.readFileSync('./Assets/IMG-20231028-WA0090.jpg')
 
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
+global.rwait = '🪽'
+global.dmoji = '☮️'
 global.done = '✅'
-global.error = '❌' 
-global.xmoji = '🔥' 
+global.error = '🪦' 
+global.xmoji = '🕊️' 
 
 global.multiplier = 69 
 global.maxwarn = '3' // máxima advertencias
